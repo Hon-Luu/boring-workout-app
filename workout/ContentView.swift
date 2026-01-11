a0e9d4b1ddb493eefa60694f76668baf881cfefb
Hon <hluu410@gmail.com>
2026-01-11 15:37:00 +0800

    Initial Commit

---
+++ workout/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  workout
+//
+//  Created by Hon Luu on 11/1/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ workout/workoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  workoutApp.swift
+//  workout
+//
+//  Created by Hon Luu on 11/1/2026.
+//
+
+import SwiftUI
+
+@main
+struct workoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
